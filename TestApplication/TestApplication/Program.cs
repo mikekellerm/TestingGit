26ee5d70f414263852c7a39a9e3ab5327fe99eda
Mike Keller <mike.kellerm@gmail.com>
2018-11-10 16:55:04 +0100

    Testing

--- TestApplication/TestApplication/Program.cs
+++ TestApplication/TestApplication/Program.cs
@@ -10,6 +10,8 @@ namespace TestApplication
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("CONSOLE IS WRITING");
+            Console.ReadKey();
         }
     }
 }
